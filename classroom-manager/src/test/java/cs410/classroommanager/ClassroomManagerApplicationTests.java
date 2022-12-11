76093d56500f198384eaac2acbbe8a9c1095f4d4
Kyle Hanson <khanson1029@gmail.com>
2022-12-11 13:01:08 -0700

    initial spring command shell

---
+++ classroom-manager/src/test/java/cs410/classroommanager/ClassroomManagerApplicationTests.java
@@ -0,0 +1,13 @@
+// package cs410.classroommanager;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class ClassroomManagerApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
